233295d8f248c65db02a0f841802c523a14ca85b
Bewerber <christian.bringmann@check24.de>
2022-09-14 12:38:00 +0200

    Regular auto-commit at 12:38:00 on 14/09/2022

--- public/includes/navigation.php
+++ public/includes/navigation.php
@@ -1,10 +1,12 @@
 <header class="p-3 text-bg-dark">
     <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start bg-white">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-left mb-md-0">
-                <li><a href="#" class="nav-link px-2 text-black">Bloglogo</a></li>
-                <li><a href="#" class="nav-link px-2 text-black">Blog NAME</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Bloglogo</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Blog NAME</a></li>
             </ul>
+
         </div>
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 
