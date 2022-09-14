3ee9a2d9070d789f3877bda41be5fd219791f582
Bewerber <christian.bringmann@check24.de>
2022-09-14 15:53:01 +0200

    Regular auto-commit at 15:53:01 on 14/09/2022

--- public/create.php
+++ public/create.php
@@ -13,21 +13,23 @@
 <?php  include "includes/navigation.php"; ?>
 
     <div class="container mt-3">
-        <div class="form-group mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Title</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-        </div>
-        <div class="form-group mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Link zum Bild</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-        </div>
-        <div class="form-group mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Text</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-        <div class="form-group">
-            <input name="create" class="btn btn-lg btn-primary btn-block" value="Create" type="submit">
-        </div>
+        <form id="login-form" role="form" autocomplete="off" class="form" method="post">
+            <div class="form-group mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Title</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="form-group mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Link zum Bild</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="form-group mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Text</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <input name="create" class="btn btn-lg btn-primary btn-block" value="Create" type="submit">
+            </div>
+        </form>
     </div>
 
 
