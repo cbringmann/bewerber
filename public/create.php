111244ca66962d7a61fbd40dc8989179c63ee9dd
Bewerber <christian.bringmann@check24.de>
2022-09-14 15:50:01 +0200

    Regular auto-commit at 15:50:01 on 14/09/2022

--- public/create.php
+++ public/create.php
@@ -14,11 +14,15 @@
 
     <div class="container mt-3">
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <label for="exampleFormControlInput1" class="form-label">Title</label>
             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <label for="exampleFormControlInput1" class="form-label">Link zum Bild</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Text</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
     </div>
