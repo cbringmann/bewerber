6bc3aa76f2652cd5aec23557e2ac0758f118a0d6
Bewerber <christian.bringmann@check24.de>
2022-09-14 07:51:01 +0200

    Regular auto-commit at 07:51:01 on 14/09/2022

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php
 
-mys
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+echo 'Connected successfully';
 
 die('Hello - Hallo');
