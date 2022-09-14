7ffdb47b6bc072e49b7fe73c44b1d93d27cf8f02
Bewerber <christian.bringmann@check24.de>
2022-09-14 15:24:01 +0200

    Regular auto-commit at 15:24:01 on 14/09/2022

--- public/index.php
+++ public/index.php
@@ -17,7 +17,14 @@
     $query = "SELECT * FROM posts";
     $select_all_posts_query = mysqli_query($conn,$query);
 
+while($row = mysqli_fetch_assoc($select_all_posts_query)) {
 
+    $post_title = $row['post_title'];
+    $post_author = $row['post_user'];
+    $post_date = $row['post_date'];
+    $post_image = $row['post_image'];
+    $post_content = substr($row['post_content'],0,400);
+    
     ?>
 
     <div class="container">
