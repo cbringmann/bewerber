87dafdc3455cfc44b81b054f25a928b3376e1aff
Bewerber <christian.bringmann@check24.de>
2022-09-14 12:02:01 +0200

    Regular auto-commit at 12:02:01 on 14/09/2022

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@
     <title>Check24 Blog</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/cms/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
 
 
