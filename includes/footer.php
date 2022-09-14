b81ab5668ed7be7a0eb77f74652b0771c53344e4
Bewerber <christian.bringmann@check24.de>
2022-09-14 12:05:01 +0200

    Regular auto-commit at 12:05:01 on 14/09/2022

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,16 @@
 
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; Blog by Check24</p>
+        </div>
+    </div>
+</footer>
+
 
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+</body>
+</html>
