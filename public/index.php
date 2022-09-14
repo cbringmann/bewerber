faea08af202987a13c01635467adceb59577e6c4
Bewerber <christian.bringmann@check24.de>
2022-09-14 07:52:01 +0200

    Regular auto-commit at 07:52:01 on 14/09/2022

--- public/index.php
+++ public/index.php
@@ -12,6 +12,6 @@ if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
 
-echo 'Connected successfully';
+echo 'Connected successfully' . PHP_EOL;
 
 die('Hello - Hallo');
