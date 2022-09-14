369ffd4f4a84e3e5e9c6aeb98520457e31407fc5
Bewerber <christian.bringmann@check24.de>
2022-09-14 15:43:01 +0200

    Regular auto-commit at 15:43:01 on 14/09/2022

--- public/create.php
+++ public/create.php
@@ -1,10 +1,21 @@
-<div class="container">
-    <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+<?php  include "includes/db.php"; ?>
+<?php  include "includes/header.php"; ?>
+
+<!-- Navigation -->
+<?php  include "includes/navigation.php"; ?>
+
+    <div class="container">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
     </div>
-    <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-</div>
+
+
+
+    <!-- Footer -->
+<?php  include "includes/footer.php"; ?>
--- public/login.php
+++ public/login.php
@@ -4,7 +4,6 @@
 
 <?php
 
-//checkIfUserIsLoggedInAndRedirect('/cms/admin');
 
 
     if(isset($_POST['username']) && isset($_POST['password'])){
