c828c7f31e42d0cb49adb7e361a8864eac0ba7b6
Bewerber <christian.bringmann@check24.de>
2022-09-14 15:27:01 +0200

    Regular auto-commit at 15:27:01 on 14/09/2022

--- public/index.php
+++ public/index.php
@@ -35,11 +35,11 @@ while($row = mysqli_fetch_assoc($select_all_posts_query)) {
         <div class="row featurette">
             <div class="col-md-7">
                 <h4 class="featurette-heading fw-normal lh-1">HEUTE, 14:11h - <span class="text-muted"><?php echo $post_title ?></span></h4>
-                <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+                <p class="lead"><?php echo $post_content ?>.</p>
+                <p class="author"><?php echo $post_author ?></p>
             </div>
             <div class="col-md-5">
                 <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-
             </div>
         </div>
 
